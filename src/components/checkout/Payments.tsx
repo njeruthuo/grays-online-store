5ff9215180bd827ef53d93629e7ca276e0481b48
Julius Njeru <127553760+njeruthuo@users.noreply.github.com>
2025-02-05 22:56:05 +0300

    websocket setup complete

--- src/components/checkout/Payments.tsx
+++ src/components/checkout/Payments.tsx
@@ -2,32 +2,38 @@ import { IPaymentType } from ".";
 import { GlobalModal } from "../modal";
 
 const Payments: React.FC<IPaymentType> = ({ open, close }) => {
-  let socket = new WebSocket("ws://127.0.0.1:8001/ws/payments/");
+  let socket = new WebSocket("ws://127.0.0.1:8001/ws/mpesa/");
 
-  socket.onopen = function () {
-    console.log("WebSocket connected");
+  socket.onopen = () => {
+    console.log("WebSocket Connected!");
   };
 
-  socket.onmessage = function (event) {
-    const paymentData = JSON.parse(event.data);
-    console.log("Payment Confirmation Received:", paymentData);
+  socket.onmessage = (event) => {
+    const data = JSON.parse(event.data);
+    console.log("MPESA Response:", data);
+    alert(`Payment Status: ${data.ResponseDescription}`);
   };
 
-  socket.onerror = function (error) {
-    console.error("WebSocket Error:", error);
-  };
+  function requestSTKPush(phone, amount, orderId) {
+    socket.send(
+      JSON.stringify({
+        phone_number: phone,
+        amount: amount,
+        order_id: orderId,
+      })
+    );
+  }
 
   socket.onclose = function () {
     console.log("WebSocket closed, reconnecting...");
     setTimeout(() => {
       reconnectWebSocket();
-    }, 3000); // Reconnect after 3 seconds
+    }, 3000);
   };
 
-  // Function to reconnect WebSocket
   function reconnectWebSocket() {
     if (socket.readyState === WebSocket.CLOSED) {
-      socket = new WebSocket("ws://127.0.0.1:8001/ws/payments/");
+      socket = new WebSocket("ws://127.0.0.1:8001/ws/mpesa/");
     }
   }
 
