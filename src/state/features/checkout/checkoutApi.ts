95fb12803f239fc0ec07ae7826dc0e9e623c04ef
Julius Njeru <127553760+njeruthuo@users.noreply.github.com>
2025-02-21 22:04:40 +0300

    set authorization headers

--- src/state/features/checkout/checkoutApi.ts
+++ src/state/features/checkout/checkoutApi.ts
@@ -5,7 +5,16 @@ export const checkoutApi = createApi({
   reducerPath: "productApi",
   baseQuery: fetchBaseQuery({
     baseUrl: BASE_URL,
+    prepareHeaders: (headers) => {
+      const token = localStorage.getItem("grayAuthToken");
+
+      if (token) {
+        headers.set("Authorization", `Bearer ${token}`);
+      }
+      return headers;
+    },
   }),
+
   tagTypes: ["Products", "Categories"],
   endpoints: (builder) => ({
     checkout: builder.mutation({
