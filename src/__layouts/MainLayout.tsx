c0f0790c8275c8c852f3a857ceb4fc5f9248ac59
Julius Njeru <127553760+njeruthuo@users.noreply.github.com>
2025-02-25 22:35:09 +0300

    main layout completion

--- src/__layouts/MainLayout.tsx
+++ src/__layouts/MainLayout.tsx
@@ -3,6 +3,8 @@ import { useDispatch } from "react-redux";
 import { Header } from "@/components/navigation";
 import { toggleShowCategories } from "@/state/features/ui/theme";
 
+import whatsappIcon from "@/assets/whatsapp-512.webp";
+
 const MainLayout = () => {
   const dispatch = useDispatch();
 
@@ -17,6 +19,21 @@ const MainLayout = () => {
       </div>
       <div className="pt-12">
         <Outlet />
+      </div>
+
+      <div className="rounded-full fixed right-1 z-40 bottom-20">
+        <a
+          href="https://wa.me/+254741627205?text=Hello!%20I%20need%20help%20with%20your%20website."
+          target="_blank"
+          rel="noopener noreferrer"
+          className="fixed right-4 bottom-20 z-40"
+        >
+          <img
+            src={whatsappIcon}
+            className="rounded-full h-12"
+            alt="WhatsApp Support"
+          />
+        </a>
       </div>
     </section>
   );
